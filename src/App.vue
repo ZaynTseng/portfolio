162793f54c622c7442cb0860dde0bb85f5e360f1
ZaynTseng <46568467+ZaynTseng@users.noreply.github.com>
2023-10-14 02:52:40 +1100

    Added sample daisyUI components
    As placeholders.

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,11 @@
 <template>
-  <h1>Hello World!</h1>
+  <the-navbar></the-navbar>
+  <the-hero></the-hero>
+  <the-footer></the-footer>
 </template>
+
+<script setup>
+import TheNavbar from "@/components/TheNavbar.vue";
+import TheHero from "@/components/TheHero.vue";
+import TheFooter from "@/components/TheFooter.vue";
+</script>
