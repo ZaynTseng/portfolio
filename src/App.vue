49530dd8ce0a6964a64f824d9c336654fe0ffcb4
ZaynTseng <46568467+ZaynTseng@users.noreply.github.com>
2023-10-17 21:13:17 +1100

    Added sidebar auto collapse
    Now the sidebar will collapse after an item is clicked.

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <template>
   <div class="drawer">
-    <input id="my-drawer-3" type="checkbox" class="drawer-toggle"/>
+    <input id="my-drawer-3" type="checkbox" class="drawer-toggle" v-model="isDrawerOpen"/>
     <div class="drawer-content flex flex-col">
       <base-navbar></base-navbar>
       <router-view></router-view>
       <base-footer></base-footer>
     </div>
-    <base-sidebar></base-sidebar>
+    <base-sidebar @itemClicked="isDrawerOpen = !isDrawerOpen"></base-sidebar>
   </div>
 </template>
+
+<script setup>
+import {ref} from "vue";
+
+const isDrawerOpen = ref(false);
+</script>
--- src/components/BaseSidebar.vue
+++ src/components/BaseSidebar.vue
@@ -3,22 +3,32 @@
     <label for="my-drawer-3" aria-label="close sidebar" class="drawer-overlay"></label>
     <ul class="menu p-4 w-80 min-h-full bg-base-100/80 backdrop-blur-xl">
       <li>
-        <router-link to="/portfolio/projects">Projects</router-link>
+        <p @click="itemClicked">Close</p>
+      </li>
+      <li></li>
+      <li>
+        <router-link to="/portfolio/projects" @click="itemClicked">Projects</router-link>
       </li>
       <li>
-        <router-link to="/portfolio/about">About Me</router-link>
+        <router-link to="/portfolio/about" @click="itemClicked">About Me</router-link>
       </li>
       <li>
-        <router-link to="/portfolio/contact">Contact</router-link>
+        <router-link to="/portfolio/contact" @click="itemClicked">Contact</router-link>
       </li>
       <li>
-        <router-link to="/portfolio/photography">Photography</router-link>
+        <router-link to="/portfolio/photography" @click="itemClicked">Photography</router-link>
       </li>
       <li>
-        <router-link to="/portfolio/more">More</router-link>
+        <router-link to="/portfolio/more" @click="itemClicked">More</router-link>
       </li>
     </ul>
   </div>
 </template>
+
 <script setup>
+const emit = defineEmits(["itemClicked"]);
+
+function itemClicked() {
+  emit("itemClicked");
+}
 </script>
