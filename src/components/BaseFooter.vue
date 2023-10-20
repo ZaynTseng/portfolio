8c2f7d37842f7a1318fa9a5108cd234b985e7fc4
ZaynTseng <46568467+ZaynTseng@users.noreply.github.com>
2023-10-20 17:44:36 +1100

    Outside links on the footer will open on a new page

--- src/components/BaseFooter.vue
+++ src/components/BaseFooter.vue
@@ -12,9 +12,9 @@
         <router-link to="/portfolio/more" class="link link-hover">More</router-link>
       </nav>
       <nav>
-        <a class="link link-hover" href="https://twitter.com/ZaynTseng">Twitter ↗</a>
-        <a class="link link-hover" href="https://github.com/ZaynTseng">Github ↗</a>
-        <a class="link link-hover" href="https://www.linkedin.com/in/zhizeng/">Linkedin ↗</a>
+        <a class="link link-hover" href="https://twitter.com/ZaynTseng" target="_blank">Twitter ↗</a>
+        <a class="link link-hover" href="https://github.com/ZaynTseng" target="_blank">Github ↗</a>
+        <a class="link link-hover" href="https://www.linkedin.com/in/zhizeng/" target="_blank">Linkedin ↗</a>
       </nav>
     </footer>
     <footer class="footer px-10 py-4 border-t border-gray-800 mx-auto max-w-5xl">
