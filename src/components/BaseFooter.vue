2c783a57f73faea11583513d615224cc8e982ef9
ZaynTseng <46568467+ZaynTseng@users.noreply.github.com>
2023-10-20 17:30:24 +1100

    Changed the footer layout, removed the bg on nav

--- src/components/BaseFooter.vue
+++ src/components/BaseFooter.vue
@@ -2,24 +2,25 @@
   <div class="bg-neutral text-neutral-content">
     <footer class="footer p-10 mx-auto max-w-5xl">
       <nav>
-        <router-link to="/portfolio/" class="footer-title hover:opacity-80">Zhi Zeng</router-link>
+        <router-link to="/portfolio/" class="link link-hover">Home</router-link>
         <router-link to="/portfolio/projects" class="link link-hover">Projects</router-link>
         <router-link to="/portfolio/about" class="link link-hover">About Me</router-link>
+      </nav>
+      <nav>
         <router-link to="/portfolio/contact" class="link link-hover">Contact</router-link>
         <router-link to="/portfolio/photography" class="link link-hover">Photography</router-link>
         <router-link to="/portfolio/more" class="link link-hover">More</router-link>
       </nav>
+      <nav>
+        <a class="link link-hover" href="https://twitter.com/ZaynTseng">Twitter ↗</a>
+        <a class="link link-hover" href="https://github.com/ZaynTseng">Github ↗</a>
+        <a class="link link-hover" href="https://www.linkedin.com/in/zhizeng/">Linkedin ↗</a>
+      </nav>
     </footer>
-    <footer
-      class="footer px-10 py-4 border-t border-gray-800 mx-auto max-w-5xl flex flex-col sm:flex-row sm:justify-between">
-      <base-social class="sm:order-last"></base-social>
-      <aside class="sm:order-first">
+    <footer class="footer px-10 py-4 border-t border-gray-800 mx-auto max-w-5xl">
+      <aside>
         <p>© 2023 Zhi Zeng. All Rights Reserved.</p>
       </aside>
     </footer>
   </div>
 </template>
-
-<script setup>
-import BaseSocial from "@/components/BaseSocial.vue";
-</script>
--- src/components/BaseNavbar.vue
+++ src/components/BaseNavbar.vue
@@ -16,8 +16,7 @@
       <div class="flex-none hidden sm:block">
         <ul class="menu menu-horizontal">
           <li v-for="(item, key) in menuItems" :key="key">
-            <router-link :to="{ path: item.path }"
-                         :class="{ 'bg-neutral text-neutral-content': currentRoute === item.path }">
+            <router-link :to="{ path: item.path }">
               {{ item.name }}
             </router-link>
           </li>
