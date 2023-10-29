b1df5ff5e1819424e6e636fb9285dd54c005ffad
ZaynTseng <46568467+ZaynTseng@users.noreply.github.com>
2023-10-30 04:40:07 +1100

    UI update

--- src/pages/LandingPage.vue
+++ src/pages/LandingPage.vue
@@ -1,22 +1,22 @@
 <template>
-  <div class="hero min-h-screen justify-start mx-auto max-w-5xl">
+  <div class="hero min-h-screen justify-start mx-auto max-w-5xl md:px-0 px-6">
     <div class="hero-content text-left">
       <div>
-        <p class="text-8xl ">Zhi Zeng
+        <p class="md:text-8xl text-7xl">Zhi Zeng
           <br>Frontend Developer
           <br>Melbourne, AU</p>
       </div>
     </div>
   </div>
-  <div class="hero min-h-screen justify-start mx-auto max-w-5xl">
+  <div class="hero min-h-screen justify-start mx-auto max-w-5xl md:px-0 px-6">
     <div class="hero-content text-left">
-      <div>
-        <p class="text-5xl my-8">Hi, I am Zhi Zeng, a frontend developer currently living in Melbourne.</p>
-        <p class="text-5xl my-8">Graduated from the University of Melbourne in 2022, I acquired great training and
+      <div class="md:text-5xl text-4xl">
+        <p class="my-8">Hi, I am Zhi Zeng, a frontend developer currently living in Melbourne.</p>
+        <p class="my-8">Graduated from the University of Melbourne in 2022, I acquired great training and
           exposure in UI/UX, machine learning, and product design.</p>
-        <p class="text-5xl my-8">As I embark on my journey in frontend development, I am eager to apply my knowledge and
+        <p class="my-8">As I embark on my journey in frontend development, I am eager to apply my knowledge and
           skills to create impactful works.</p>
-        <div class="text-4xl inline-flex items-end text-blue-500">
+        <div class="md:text-4xl text-3xl mb-40 inline-flex items-end text-blue-500">
           <router-link to="/portfolio/projects" class="link link-hover">My Projects</router-link>
           <svg xmlns="http://www.w3.org/2000/svg" class="w-8 h-8" viewBox="0 0 512 512">
             <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="48"
