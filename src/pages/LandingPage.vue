0e0e8ca1be1598bb41ffea1c5617782434ce2f8c
ZaynTseng <46568467+ZaynTseng@users.noreply.github.com>
2023-10-30 16:41:18 +1100

    Content update

--- src/pages/LandingPage.vue
+++ src/pages/LandingPage.vue
@@ -2,7 +2,7 @@
   <div class="hero min-h-screen justify-start mx-auto max-w-5xl md:px-0 px-6">
     <div class="hero-content text-left">
       <div>
-        <p class="md:text-8xl text-7xl">Zhi Zeng
+        <p class="md:text-8xl sm:text-7xl text-6xl">Zhi Zeng
           <br>Frontend Developer
           <br>Melbourne, AU</p>
       </div>
@@ -10,13 +10,13 @@
   </div>
   <div class="hero min-h-screen justify-start mx-auto max-w-5xl md:px-0 px-6">
     <div class="hero-content text-left">
-      <div class="md:text-5xl text-4xl">
+      <div class="md:text-5xl sm:text-4xl text-3xl">
         <p class="my-8">Hi, I am Zhi Zeng, a frontend developer currently living in Melbourne.</p>
         <p class="my-8">Graduated from the University of Melbourne in 2022, I acquired great training and
           exposure in UI/UX, machine learning, and product design.</p>
-        <p class="my-8">As I embark on my journey in frontend development, I am eager to apply my knowledge and
-          skills to create impactful works.</p>
-        <div class="md:text-4xl text-3xl mb-40 inline-flex items-end text-blue-500">
+        <p class="my-8">Embarking on the journey of frontend development, I am eager to leverage my knowledge and skills
+          to create impactful works.</p>
+        <div class="md:text-4xl sm:text-3xl text-2xl mb-40 inline-flex items-end text-blue-500">
           <router-link to="/portfolio/projects" class="link link-hover">My Projects</router-link>
           <svg xmlns="http://www.w3.org/2000/svg" class="w-8 h-8" viewBox="0 0 512 512">
             <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="48"
