dea3a5359e8a7eb484fddb373a54b0fc78b21aa3
ZaynTseng <46568467+ZaynTseng@users.noreply.github.com>
2023-11-06 18:29:35 +1100

    Update ProjectStereoDisparity.vue

--- src/pages/projects/ProjectStereoDisparity.vue
+++ src/pages/projects/ProjectStereoDisparity.vue
@@ -1,3 +1,23 @@
 <template>
+  <base-hero>
+    <div class="max-w-3xl gap-2 grid">
+      <figure>
+        <img src="../../assets/projects/stereo-disparity/stereo-disparity-cover.png" alt="Stereo Disparity"
+             class="border-base-content bg-base-300 rounded-box border border-opacity-5">
+      </figure>
+      <h1 class="text-5xl font-bold pt-4">Stereo Disparity</h1>
+      <p class="text-2xl">Stereo disparity algorithm experiments and evaluation</p>
+      <div class="gap-2 flex pb-6">
+        <span class="badge badge-primary">Algorithms</span>
+        <span class="badge badge-secondary">Computer Vision</span>
+        <span class="badge badge-ghost">October 2022</span>
+      </div>
 
+
+    </div>
+  </base-hero>
 </template>
+
+<script setup>
+import BaseHero from "@/components/BaseHero.vue";
+</script>
