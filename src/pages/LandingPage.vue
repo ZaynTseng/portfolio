a23ee60112a4e0eb5a6ed30039d8ba688525c7ef
ZaynTseng <46568467+ZaynTseng@users.noreply.github.com>
2023-11-08 01:49:50 +1100

    Added non break spaces

--- src/pages/LandingPage.vue
+++ src/pages/LandingPage.vue
@@ -4,18 +4,18 @@
       class="flex flex-col justify-center min-h-[80vh] w-[80vw] py-20
       text-5xl sm:text-6xl md:text-7xl lg:text-8xl">
       <p>Zhi Zeng</p>
-      <p>Frontend Developer</p>
+      <p>Frontend&nbsp;Developer</p>
       <p>Melbourne, AU</p>
     </div>
     <div
       class="flex flex-col justify-center min-h-[80vh] w-[80vw] py-20
       text-2xl sm:text-3xl md:text-4xl lg:text-5xl
       gap-4 sm:gap-5 md:gap-6 lg:gap-7">
-      <p>Hi, I am Zhi Zeng, a frontend developer currently living in Melbourne.</p>
+      <p>Hi, I am Zhi Zeng, a frontend developer currently living in&nbsp;Melbourne.</p>
       <p>Graduated from the University of Melbourne in 2022, I acquired great exposure and exercise in
-        UI/UX, software development, machine learning, and product design.</p>
+        UI/UX, software development, machine learning, and product&nbsp;design.</p>
       <p>Embarking on the journey of frontend development, I am eager to apply my knowledge and skills to
-        create impactful works.</p>
+        create impactful&nbsp;works.</p>
       <div class="text-xl sm:text-2xl md:text-3xl lg:text-4xl flex text-blue-500 gap-1">
         <router-link to="/portfolio/projects" class="link link-hover">My Projects</router-link>
         <p>→</p>
--- src/pages/Projects.vue
+++ src/pages/Projects.vue
@@ -4,8 +4,7 @@
                  class="flex flex-col items-center justify-between w-full min-h-[50vh] max-h-[80vh] bg-base-200 overflow-hidden">
       <div class="flex flex-col mt-16 mb-4 items-center gap-y-1 md:gap-y-1.5 lg:gap-y-2">
         <h1 class="text-4xl md:text-5xl lg:text-6xl font-medium">Plant Mate</h1>
-        <p class="text-xl md:text-2xl lg:text-3xl font-light text-center">A digital twin app crafted for novice urban
-          farmers</p>
+        <p class="text-xl md:text-2xl lg:text-3xl font-light text-center">A digital twin app crafted for novice urban&nbsp;farmers</p>
         <div class="text-md md:text-lg lg:text-xl flex text-blue-500 gap-1">
           <p class="link link-hover">Read</p>
           <p>→</p>
@@ -20,7 +19,7 @@
                  class="flex items-center justify-between w-full min-h-[50vh] max-h-[80vh] bg-[#EAE3DC] overflow-hidden">
       <div class="flex flex-col ml-12 md:ml-20 lg:ml-28 mr-8 gap-y-1 md:gap-y-1.5 lg:gap-y-2">
         <h1 class="text-4xl md:text-5xl lg:text-6xl font-medium">RecipeScale</h1>
-        <p class="text-xl md:text-2xl lg:text-3xl font-light">Store, share, and learn <br>recipes with ease</p>
+        <p class="text-xl md:text-2xl lg:text-3xl font-light">Store, share, and learn recipes with&nbsp;ease</p>
         <div class="text-md md:text-lg lg:text-xl flex text-blue-500 gap-1">
           <p class="link link-hover">Read</p>
           <p>→</p>
@@ -35,8 +34,7 @@
                  class="flex flex-col items-center justify-between w-full min-h-[50vh] max-h-[80vh] bg-base-200 overflow-hidden">
       <div class="flex flex-col mt-16 mb-8 items-center gap-y-1 md:gap-y-1.5 lg:gap-y-2">
         <h1 class="text-4xl md:text-5xl lg:text-6xl font-medium">Stereo Disparity</h1>
-        <p class="text-xl md:text-2xl lg:text-3xl font-light text-center">Stereo disparity algorithm experiments and
-          evaluation</p>
+        <p class="text-xl md:text-2xl lg:text-3xl font-light text-center">Stereo disparity algorithm experiments and&nbsp;evaluation</p>
         <div class="text-md md:text-lg lg:text-xl flex text-blue-500 gap-1">
           <p class="link link-hover">Read</p>
           <p>→</p>
