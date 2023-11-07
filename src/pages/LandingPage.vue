4537ca89f24fafee28dcc6c52211cc75dbad803a
ZaynTseng <46568467+ZaynTseng@users.noreply.github.com>
2023-11-08 01:29:43 +1100

    Landing page update

--- src/pages/LandingPage.vue
+++ src/pages/LandingPage.vue
@@ -1,28 +1,25 @@
 <template>
-  <div class="hero min-h-screen justify-start mx-auto max-w-5xl md:px-0 px-6">
-    <div class="hero-content text-left">
-      <div>
-        <p class="md:text-8xl sm:text-7xl text-6xl">Zhi Zeng
-          <br>Frontend Developer
-          <br>Melbourne, AU</p>
-      </div>
+  <div class="flex flex-col items-center">
+    <div
+      class="flex flex-col justify-center min-h-[80vh] w-[80vw] py-20
+      text-5xl sm:text-6xl md:text-7xl lg:text-8xl">
+      <p>Zhi Zeng</p>
+      <p>Frontend Developer</p>
+      <p>Melbourne, AU</p>
     </div>
-  </div>
-  <div class="hero min-h-screen justify-start mx-auto max-w-5xl md:px-0 px-6">
-    <div class="hero-content text-left">
-      <div class="md:text-5xl sm:text-4xl text-3xl">
-        <p class="my-8">Hi, I am Zhi Zeng, a frontend developer currently living in Melbourne.</p>
-        <p class="my-8">Graduated from the University of Melbourne in 2022, I acquired great exposure and exercise in
-          UI/UX, software development, machine learning, and product design.</p>
-        <p class="my-8">Embarking on the journey of frontend development, I am eager to apply my knowledge and skills to
-          create impactful works.</p>
-        <div class="text-2xl sm:text-3xl md:text-4xl mb-40 flex text-blue-500 gap-1">
-          <router-link to="/portfolio/projects" class="link link-hover">My Projects</router-link>
-          <p>→</p>
-        </div>
+    <div
+      class="flex flex-col justify-center min-h-[80vh] w-[80vw] py-20
+      text-2xl sm:text-3xl md:text-4xl lg:text-5xl
+      gap-4 sm:gap-5 md:gap-6 lg:gap-7">
+      <p>Hi, I am Zhi Zeng, a frontend developer currently living in Melbourne.</p>
+      <p>Graduated from the University of Melbourne in 2022, I acquired great exposure and exercise in
+        UI/UX, software development, machine learning, and product design.</p>
+      <p>Embarking on the journey of frontend development, I am eager to apply my knowledge and skills to
+        create impactful works.</p>
+      <div class="text-xl sm:text-2xl md:text-3xl lg:text-4xl flex text-blue-500 gap-1">
+        <router-link to="/portfolio/projects" class="link link-hover">My Projects</router-link>
+        <p>→</p>
       </div>
     </div>
   </div>
 </template>
-<script setup>
-</script>
